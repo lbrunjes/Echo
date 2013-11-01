e9104c397f619cb75db08fd960e3ac4b34c61637
lee <lee@housemark.co>
2013-11-01 12:51:08 -0400

    no multi runs

--- Server/Main.cs
+++ Server/Main.cs
@@ -15,43 +15,51 @@ namespace Server
 	{
 		public static void Main (String[] args)
 		{
-			bool loop = true;
-			if (args.Length > 0 && args [0].ToLower () == "once") {
-				loop = false;
-			}
+			using (Mutex soloChk = new Mutex(false, System.Reflection.Assembly.GetCallingAssembly().GetName().FullName)) {
 
-
-			//read teh settings file
-			Settings.ReadConfigFile (Settings.CONFIG_FILE_SERVER);
-
-			Console.Write ("Starting up");
-
-			System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch ();
-			sw.Start ();
-			SyncList Listing = new SyncList (Settings.LocalDirectory);
-
-			//Loop occasionally
-			do{
-
-				//write the hashes file to the server directory.
-				Listing.saveSyncList (Settings.HashFile);
-
-				Console.WriteLine (" ... Done ("+ sw.ElapsedMilliseconds+" ms)");
-				sw.Stop ();
-
-				if(loop){	
-					//wait a bit.
-					Thread.Sleep (Settings.LoopTime);
-					sw.Reset ();
-					sw.Start ();
-
-					//
-					Console.Write ("Scanning");
-
-					//reset the hashes
-					Listing.loadSyncList (Listing.Path);
+				if(!soloChk.WaitOne(0, false)){
+					Console.WriteLine("ERROR: Another server instance is running. So we are not going to run. sorry.");
+					return;
 				}
-			}while(loop);
+				bool loop = true;
+				if (args.Length > 0 && args [0].ToLower () == "once") {
+					loop = false;
+				}
+
+
+				//read teh settings file
+				Settings.ReadConfigFile (Settings.CONFIG_FILE_SERVER);
+
+				Console.Write ("Starting up");
+
+				System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch ();
+				sw.Start ();
+				SyncList Listing = new SyncList (Settings.LocalDirectory);
+
+				//Loop occasionally
+				do {
+
+					//write the hashes file to the server directory.
+					Listing.saveSyncList (Settings.HashFile);
+
+					Console.WriteLine (" ... Done (" + sw.ElapsedMilliseconds + " ms)");
+					sw.Stop ();
+
+					if (loop) {	
+						//wait a bit.
+						Thread.Sleep (Settings.LoopTime);
+						sw.Reset ();
+						sw.Start ();
+
+						//
+						Console.Write ("Scanning");
+
+						//reset the hashes
+						Listing.loadSyncList (Listing.Path);
+					}
+				} while(loop);
+
+			}
 		}
 	}
 }
